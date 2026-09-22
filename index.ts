f795975bf3c6ae1106c713d266f624190f6c6813
Sandeep Gangadhar <sanndeepgc@gmail.com>
2026-09-22 15:04:12 +0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import {Student} from './classes/student'
+import {personPrototype} from "./classes/objectprototype";
+
+const message: string = "Congratulations, this is your node project";
+console.log(message);
+console.debug("This is how debugger looks like");
+console.error("This is how error log looks like");
+
+
+console.log("Object creation from the external file")
+const sandeep: Student = new Student("Sandeep G");
+console.log(sandeep);
+console.debug(sandeep.getName());
+
+
+console.log("Object instantiation of Object Prototype ")
+let john = Object.create(personPrototype);
+john.greet();
+john.name = "John";
+john.age = 36;
+console.log(john);
+john.greet()
